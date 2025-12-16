463c8fc4f038fe172a717130c060a2d663583e3f
yakinemejai <YAKINE.MEJAI@latoile.em-lyon.com>
2025-12-16 16:02:57 +0100

    MODIF

--- script.js
+++ script.js
@@ -1,45 +1,86 @@
 //  Faire le HTML - structure (header, main et footer, nav avec lien cliquable)
 
 //ETAPE 1 : construire l'URL de L'API
+const API_URL = `https://js-dynamic-portfolio-data-makerslab-emlyon-cdweb-8f83155c64a0cc.gitlab.io/json/menuiserie.json`;
 
 
 
-        // ETAPE 2 : Parcourir le tableau JSON (mettre le fetch)
-        fetch("Menuiserie Bobois.json")
-  .then(response => response.json())
-  .then(data => {
 
-    // verifier si ca marche ou pas 
+// ETAPE 2 : Parcourir le tableau JSON (mettre le fetch)
+fetch(API_URL)
+    .then(response => response.json())
+    .then(data => {
 
-console.log();
-
-        // Etape 3 : nom de l'entreprise (h1), 
-
-        // slogan (H2) et les boutons
+        // verifier si ca marche ou pas 
+        console.log(data.realisations);
 
 
-        // ETAPE 4 : Pour chaque élement du tableau "réalisation"créer une DIV
+        data.realisations.forEach(element => {
 
-        // - Créer un titre (h3) pour afficher le nom
+            // Etape 3 : nom de l'entreprise (h1),slogan (H2) et les boutons
 
-        // - Créer un paragraphe (p) pour la description
 
-        // - Créer un élément image (img) pour afficher la photo
 
-        // ETAPE 5 : Envoyer dans le HTML les élements crées (appenchild)
+            // ETAPE 4 : Pour chaque élement du tableau "réalisation"créer une DIV
 
+
+            // - Créer un titre (h3) pour afficher le nom
+            let title = document.createElement("h3");
+            title.textContent = element.title
+            console.log(title);
+            // - Créer un paragraphe (p) pour la description
+            let paragraphe = document.createElement("p");
+            paragraphe.textContent = element.description
+            console.log(paragraphe);
+
+
+            // - Créer un élément image (img) pour afficher la photo
+
+            let image = document.createElement("img");
+            image.src = element["image-url"];
+            console.log(image);
+
+
+            // ETAPE 5 : Envoyer dans le HTML les élements crées (appenchild)
+
+            //  .appendChild();
+            //   .appendChild();
+            //   .appendChild();
+            //   .appendChild();
+
+        });
         // ETAPE 6 : Pour chaque élement du tableau "témoignages"créer une DIV
-
-        // - Créer un titre (h3) pour afficher le prénom
-
-        // - Créer un titre (h4) pour afficher le type de prestation
-
-        // - Créer un paragraphe (p) pour les commentaires
-
-        // ETAPE 7 : Envoyer dans le HTML les élements crées (appenchild)
+        data.temoignages.forEach(temoi => {
 
 
 
+            // - Créer un titre (h3) pour afficher le prénom
+            let name = document.createElement("h3")
+            name.textContent = temoi.prenom
+            console.log(name);
+            
+            // - Créer un titre (h4) pour afficher le type de prestation
+            let presta = document.createElement("h4")
+            presta.textContent = temoi.typePrestation
+console.log(presta);
 
-          });
-      
+
+            // - Créer un paragraphe (p) pour les commentaires
+          
+let commantaire = document.createElement("p")
+commantaire.textContent = temoi.note
+console.log(commantaire);
+
+            // ETAPE 7 : Envoyer dans le HTML les élements crées (appenchild)
+            
+
+
+            
+              //  .appendChild(img);
+            //   .appendChild(title);
+            //   .appendChild(priceElt);
+            //   .appendChild(desc);
+
+        });
+
+    });
