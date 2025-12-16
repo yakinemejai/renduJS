bd648cb0dce3c87657fd4b10dcdf32c4f5f2ef33
yakinemejai <YAKINE.MEJAI@latoile.em-lyon.com>
2025-12-16 17:21:54 +0100

    ok

--- script.js
+++ script.js
@@ -11,15 +11,44 @@ fetch(API_URL)
     .then(response => response.json())
     .then(data => {
 
+        // Hero section
+
+
+        let accroche = document.getElementById("accroche");
+
+        let card = document.createElement("div");
+        // Etape 3 : nom de l'entreprise (h1),slogan (H2) et les boutons
+        let titre = document.createElement("h2");
+        titre.textContent = data.entreprise;
+        let slogan = document.createElement("p");
+        slogan.textContent = data.propositionDeValeur;
+        let boutton = document.createElement("button");
+        boutton.textContent = data.texteBouton;
+
+        accroche.appendChild(titre);
+        accroche.appendChild(slogan);
+        accroche.appendChild(boutton);
+        
+
+
+
         // verifier si ca marche ou pas 
         console.log(data.realisations);
+        console.log(data.temoignages);
 
+        data.promessesClients.forEach(promesse => {
+
+            let promesses = document.createElement("p")
+            promesses.textContent = promesse
+            console.log(promesses);
+        });
+
+        const logo = document.getElementById("logo")
+
+
+        // Section realisations
 
         data.realisations.forEach(element => {
-
-            // Etape 3 : nom de l'entreprise (h1),slogan (H2) et les boutons
-
-
 
             // ETAPE 4 : Pour chaque élement du tableau "réalisation"créer une DIV
 
@@ -43,44 +72,41 @@ fetch(API_URL)
 
             // ETAPE 5 : Envoyer dans le HTML les élements crées (appenchild)
 
-            //  .appendChild();
-            //   .appendChild();
-            //   .appendChild();
-            //   .appendChild();
-
+            container.appendChild(card);
+            card.appendChild(title);
+            card.appendChild(paragraphe);
+            card.appendChild(image);
         });
-        // ETAPE 6 : Pour chaque élement du tableau "témoignages"créer une DIV
-        data.temoignages.forEach(temoi => {
 
+
+        // Section Temoignages
+
+        data.temoignages.forEach(temoi => {
 
 
             // - Créer un titre (h3) pour afficher le prénom
             let name = document.createElement("h3")
             name.textContent = temoi.prenom
             console.log(name);
-            
+
             // - Créer un titre (h4) pour afficher le type de prestation
             let presta = document.createElement("h4")
             presta.textContent = temoi.typePrestation
-console.log(presta);
+            console.log(presta);
 
 
             // - Créer un paragraphe (p) pour les commentaires
-          
-let commantaire = document.createElement("p")
-commantaire.textContent = temoi.note
-console.log(commantaire);
+
+            let commantaire = document.createElement("p")
+            commantaire.textContent = temoi.note
+            console.log(commantaire);
 
             // ETAPE 7 : Envoyer dans le HTML les élements crées (appenchild)
-            
 
 
-            
-              //  .appendChild(img);
-            //   .appendChild(title);
-            //   .appendChild(priceElt);
-            //   .appendChild(desc);
+
 
         });
+        
 
     });
