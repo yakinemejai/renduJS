ed321e08f9b54d15ae1c1031c4fb6573635dd8d1
yakinemejai <YAKINE.MEJAI@latoile.em-lyon.com>
2025-12-16 12:28:28 +0100

    yes

--- script.js
+++ script.js
@@ -1,23 +1,45 @@
-// ===== ÉTAPE 1 : Cree l'html  =====
+//  Faire le HTML - structure (header, main et footer, nav avec lien cliquable)
+
+//ETAPE 1 : construire l'URL de L'API
 
 
-// ===== ÉTAPE 2: construire l'URL de L'API  =====
 
-// ===== ÉTAPE 3: reupere les donnees (fetch) ficher json =====
-fetch("TroVit - VTC Grenoble.json")
+        // ETAPE 2 : Parcourir le tableau JSON (mettre le fetch)
+        fetch("Menuiserie Bobois.json")
   .then(response => response.json())
   .then(data => {
-// je dois coder a l'intérieur du fetch 
-   });
 
- // ===== ÉTAPE 4: tester si ca marche ou pas 
- console.log(data.VTC);
+    // verifier si ca marche ou pas 
 
- // ===== ÉTAPE 5:  parcourir la liste (data.VTC.forEach(element =>)
+console.log();
 
-  // ===== ÉTAPE  6 : pour chaque voyage : (creat elemente )
-    // a- Créer un titre (h3) pour afficher le nom
-    // b- Créer un paragraphe (p)
-    // c- Créer un titre (h4)
-    // d- 
- // ===== ÉTAPE  7 : Créer une div (card) pour englober les elements qui suivent
+        // Etape 3 : nom de l'entreprise (h1), 
+
+        // slogan (H2) et les boutons
+
+
+        // ETAPE 4 : Pour chaque élement du tableau "réalisation"créer une DIV
+
+        // - Créer un titre (h3) pour afficher le nom
+
+        // - Créer un paragraphe (p) pour la description
+
+        // - Créer un élément image (img) pour afficher la photo
+
+        // ETAPE 5 : Envoyer dans le HTML les élements crées (appenchild)
+
+        // ETAPE 6 : Pour chaque élement du tableau "témoignages"créer une DIV
+
+        // - Créer un titre (h3) pour afficher le prénom
+
+        // - Créer un titre (h4) pour afficher le type de prestation
+
+        // - Créer un paragraphe (p) pour les commentaires
+
+        // ETAPE 7 : Envoyer dans le HTML les élements crées (appenchild)
+
+
+
+
+          });
+      
